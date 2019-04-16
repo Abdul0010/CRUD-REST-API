4d99e4b43f0c562971fc0c582573a84e4cc12787
abdul <abdulqaderutm@gmail.com>
2019-04-16 20:37:50 +0800

    first commit

---
+++ src/main/java/com/masafat/MasafatApplication.java
@@ -0,0 +1,13 @@
+package com.masafat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasafatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasafatApplication.class, args);
+	}
+
+}
